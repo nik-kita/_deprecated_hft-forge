9486caf183ab767d4549e5cf4358311b6e32b349
nik <nikita.nodejs@gmail.com>
2023-02-14 16:12:24 +0100

    KuWsClient.connect()

--- libs/ku-ws-api/src/lib/ku-ws-api.ku-ws.client.ts
+++ libs/ku-ws-api/src/lib/ku-ws-api.ku-ws.client.ts
@@ -13,4 +13,32 @@ export class KuWsClient {
 
         return WS_READY_STATE_V_K[this.ws.readyState];
     }
+
+    connect(url: `ws${string}`) {
+        return new Promise<void>((resolve, reject) => {
+            const wsState = this.getWsState() || 'OPEN';
+            const open = () => {
+                this.ws = new WebSocket(url);
+                this.ws.once('open', resolve);
+            };
+            const onClose = () => new Promise((rs, rj) => {
+                this.ws.once('close', rs);
+                this.ws.close();
+            });
+            const resolver: Record<WsReadyState, () => void> = {
+                OPEN: () => {
+                    if (this.ws.url === url) {
+                        resolve();
+                    } else {
+                        onClose().then(open);
+                    }
+                },
+                CLOSED: open,
+                CLOSING: () => onClose().then(open),
+                CONNECTING: () => this.ws.on('open', resolve),
+            };
+
+            resolver[wsState]();
+        });
+    }
 }
