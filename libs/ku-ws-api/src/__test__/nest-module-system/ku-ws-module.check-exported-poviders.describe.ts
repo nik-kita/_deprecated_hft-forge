bf1cc38a2506a96a23485ce5decf50f954bcd10f
nik <nikita.nodejs@gmail.com>
2023-02-16 06:25:52 +0100

    add test

--- libs/ku-ws-api/src/__test__/nest-module-system/ku-ws-module.check-exported-poviders.describe.ts
+++ libs/ku-ws-api/src/__test__/nest-module-system/ku-ws-module.check-exported-poviders.describe.ts
@@ -2,6 +2,7 @@ import { WsClientService } from '@hft-forge/ws';
 import { describe, expect, it } from '@jest/globals';
 import { Module } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { HttpService } from '@hft-forge/http';
 import { KuWsApiModule } from '../../lib/ku-ws-api.module';
 import { KuWsApiService } from '../../lib/ku-ws-api.service';
 
@@ -38,6 +39,22 @@ export const describe_ku_ws_module_exported_providers_check = (
 
             expect(kuWsApiService).toBeInstanceOf(KuWsApiService);
             expect((kuWsApiService as any)['wsClient']).toBeInstanceOf(WsClientService);
+
+            await clientApp?.close();
+        });
+
+        it(`${KuWsApiService.name} should contain /httpClient/ property ${HttpService.name}`, async () => {
+            const clientApp = await Test.createTestingModule({
+                imports: [ClientModule],
+            }).compile();
+
+            const httpService = clientApp.get(HttpService);
+
+            expect(httpService).toBeInstanceOf(HttpService);
+
+            const kuWsApiService = clientApp.get(KuWsApiService);
+
+            expect((kuWsApiService as any)['httpClient']).toBeInstanceOf(HttpService);
 
             await clientApp?.close();
         });
--- libs/ku-ws-api/src/lib/ku-ws-api.service.ts
+++ libs/ku-ws-api/src/lib/ku-ws-api.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { HttpService } from '@hft-forge/http';
 import { WsClientService } from '@hft-forge/ws';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class KuWsApiService {
-    constructor(private wsClient: WsClientService) {}
+    constructor(
+        private wsClient: WsClientService,
+        private httpClient: HttpService,
+    ) {}
 }
