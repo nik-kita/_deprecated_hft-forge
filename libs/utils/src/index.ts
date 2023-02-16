75967afd0f2c0e88933cd0b6cdc8b1c1a0d70d9a
nik <nikita.nodejs@gmail.com>
2023-02-16 02:03:18 +0100

    utils: genMockConfigModule()

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,7 +1,9 @@
+export { BindThis } from './bind-this.decorator.util';
+export { genMockConfigModule } from './config.module.nest.for-test.util';
+export { describePortal } from './describe-portal.for-test.util';
 export { genUniqueStr } from './gen-uniqu-str.util';
+export { HftForgeError } from './hft-forge.error.util';
 export { itif } from './itif.for-test.util';
 export { qsFromObj } from './qs-from-obj.util';
 export { SubMessageLikeMethod } from './sub-message-like-method.decorator.util';
-export { BindThis } from './bind-this.decorator.util';
-export { HftForgeError } from './hft-forge.error.util';
-export { describePortal } from './describe-portal.for-test.util';
+
