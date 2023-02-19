c687702c3588b1b8d04826eb56b2c9bd7013ad1d
nik <nikita.nodejs@gmail.com>
2023-02-19 04:16:10 +0100

    fix

---
+++ libs/ku-ws-api/src/__test__/real-execution/welcome.spec.ts
@@ -0,0 +1,85 @@
+import { HttpService } from '@hft-forge/http';
+import { itif } from '@hft-forge/test-pal/core';
+import {
+    KuRes_apply_connect_token,
+    KuWsResType, KuWsRes_welcome, KU_BASE_URL,
+    KU_ENV_KEYS,
+    KU_POST_ENDPOINT
+} from '@hft-forge/types/ku';
+import { WsClientService } from '@hft-forge/ws';
+
+
+describe('Should subscribe and receive "welcome" in "type" property of response', () => {
+    let wsClient: WsClientService;
+    let httpClient: HttpService;
+
+
+    beforeEach(async () => {
+        wsClient = new WsClientService();
+        httpClient = new HttpService();
+    });
+
+    itif({
+        needEnv: { envFilePath: '.test.env', envVariables: KU_ENV_KEYS.map((k) => k) },
+    })('Should subscribe and receive "welcome" message', async () => {
+        let wasHere = 'where?';
+        const already = 'already';
+
+        const offTimeout = setTimeout(() => {
+            expect('not').toBe('here');
+        }, 7_000);
+
+        const { body } = await httpClient
+            .req(
+                `${KU_BASE_URL}${KU_POST_ENDPOINT.apply_ws_connect_token.public}`,
+                { method: 'POST' },
+            );
+
+
+        expect(body?.json).toBeInstanceOf(Function);
+
+        const jBody = await body.json() as KuRes_apply_connect_token;
+
+        expect(jBody.code).toBe('200000' satisfies KuRes_apply_connect_token['code']);
+        expect(jBody.data.token).toMatch(/\S+/);
+
+        const endpoint = jBody.data.instanceServers[0]?.endpoint;
+        const token = jBody.data.token;
+
+        expect(endpoint).toMatch(/^wss/);
+
+        await wsClient.connect(endpoint, {
+            token,
+            id: Date.now(),
+        });
+
+        const originWs = wsClient.__getOriginWs();
+
+        originWs.on('message', (data) => {
+            wasHere = already;
+
+            const message = JSON.parse(data.toString()) as KuWsRes_welcome;
+
+            expect(message.type).toBe('welcome' satisfies KuWsResType);
+            expect(message.id).toBeDefined();
+        });
+
+        expect(wsClient.getWsState()).toBe('OPEN');
+
+        await new Promise<void>((resolve) => setTimeout(resolve, 3_000));
+
+        await wsClient.disconnect();
+
+        expect(wsClient.getWsState()).toBe('CLOSED');
+
+        clearTimeout(offTimeout);
+
+        expect(wasHere).toBe(already);
+    }, 15_000);
+
+    afterEach(async () => {
+        await wsClient?.disconnect();
+    });
+});
+
+
