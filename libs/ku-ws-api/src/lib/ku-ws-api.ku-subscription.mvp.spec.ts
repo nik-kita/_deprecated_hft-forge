d147289cbabff42cf416a120aa7d25201185b9b6
nik <nikita.nodejs@gmail.com>
2023-02-23 01:59:25 +0100

    add tests

--- libs/ku-ws-api/src/lib/ku-ws-api.ku-subscription.mvp.spec.ts
+++ libs/ku-ws-api/src/lib/ku-ws-api.ku-subscription.mvp.spec.ts
@@ -1,10 +1,13 @@
+import { CurrencyPair } from '@hft-forge/types/ku/common';
 import { PrivacyStatus, SubscriptionStatus } from '@hft-forge/types/ku/ws';
+import { HftForgeError } from '@hft-forge/utils';
 import { KuSubscriptionManager } from './ku-ws-api.ku-subscription-manager';
 
 
 
 describe(KuSubscriptionManager.name, () => {
     const id = `LEVEL_2::${Date.now()}` as const;
+    const currency: CurrencyPair = '1EARTH-USDT';
     it('Should not get manager', () => {
         const manager = KuSubscriptionManager.getById('LEVEL_2::1');
 
@@ -34,7 +37,7 @@ describe(KuSubscriptionManager.name, () => {
         expect(manager.id).toBe(id);
         expect(manager.topic).toBeDefined();
         expect(manager.topic.length).toBe(1);
-        expect(manager.topic[0]).toBe('1EARTH-USDT');
+        expect(manager.topic[0]).toBe(currency);
         expect(manager.isTopicIncluded).toBe(true);
         expect(manager.status).toBe('pending');
         expect(manager.privacyStatus).toBe('private-public' satisfies PrivacyStatus);
@@ -42,9 +45,35 @@ describe(KuSubscriptionManager.name, () => {
 
     it('Should update "status" to "active"', () => {
         const manager = KuSubscriptionManager.getById(id) as any;
-
+        
         manager.updateByAck();
-
+        
         expect(manager.status).toBe('active' satisfies SubscriptionStatus);
+    });
+    
+    it('Should throw error because "ack" happen without client request', () => {
+        const manager = KuSubscriptionManager.getById(id) as any;
+        
+        expect(() => manager.updateByAck()).toThrowError(HftForgeError);
+    });
+    
+    it ('Should unsubscribe and set correct values', () => {
+        const manager = KuSubscriptionManager.getById(id);
+
+        manager?.updateByClient({
+            id,
+            channel: 'LEVEL_2',
+            privateChannel: true,
+            type: 'unsubscribe',
+            response: true,
+            topic_first_part: '/market/level2:',
+            topic_second_splitted_by_comma_part: [currency],
+        });
+
+        const _manager = manager as any;
+
+        expect(_manager.status).toBe('changing');
+        expect(_manager.privacyStatus).toBe('public-only');
+        expect(_manager.topic.length).toBe(0);
     });
 });
